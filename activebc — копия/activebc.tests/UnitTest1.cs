88148a0aec2e2c9f2c7c08a12100cc36b109ea17
boevas <boev_a@bk.ru>
2021-11-03 11:54:39 +0300

    Tests

---
+++ activebc — копия/activebc.tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using Xunit;
+
+namespace activebc.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            dynamic json = new ExpandoObject();
+            json.Port = 1111;
+            json.IPAddress = "127.0.0.1";
+
+            dynamic server1 = new ExpandoObject();
+            server1.IPAdress = "127.0.0.1";
+            server1.Port = 123;
+
+            dynamic server2 = new ExpandoObject();
+            server2.IPAdress = "127.0.0.1";
+            server2.Port = 456;
+
+            dynamic server3 = new ExpandoObject();
+            server3.IPAdress = "127.0.0.1";
+            server3.Port = 789;
+
+            json.Servers = new List<dynamic>() { server1, server2, server3 };
+            string jsonString = JsonSerializer.Serialize(json);
+
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString)))
+            {
+                var configuration = new ConfigurationBuilder().AddJsonStream(ms).Build();
+            }
+        }
+    }
+}
---
+++ activebc — копия/activebc/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace activebc
+{
+    public class Server
+    {
+        public string IPAdress{ get; set; }
+        public ushort Port { get; set; }
+        public int CountrediRection { get; set; }
+    }
+}
