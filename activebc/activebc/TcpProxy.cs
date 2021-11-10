ab517546bf371c24c92033c3b5b81e96bf38dd12
boevas <boev_a@bk.ru>
2021-11-10 15:27:20 +0300

    try catch

--- activebc/activebc/TcpProxy.cs
+++ activebc/activebc/TcpProxy.cs
@@ -12,7 +12,7 @@ namespace activebc
     public class TcpProxy
     {
         private TcpClient client;
-        private readonly TcpClient server = new();
+        private TcpClient server;
         private readonly Сonfiguration Сonfiguration;
         private readonly ILogger<TcpProxy> Log;
 
@@ -21,58 +21,81 @@ namespace activebc
             Сonfiguration = configuration.Get<Сonfiguration>();
             Log = log;
         }
-        public TcpProxy(Сonfiguration configuration, ILogger<TcpProxy> log)
-        {
-            Сonfiguration = configuration;
-            Log = log;
-        }
         public void Start()
         {
-            Log.LogInformation($"Start service Proxy");
-            var listner = new TcpListener(IPAddress.Parse(Сonfiguration.IPAddress), Сonfiguration.Port);
-            listner.Start();
-            while (true)
-                new TcpProxy(Сonfiguration, Log).Start(listner.AcceptTcpClient()).Wait();
+            try
+            {
+                Log.LogInformation($"Start service Proxy");
+                var listner = new TcpListener(IPAddress.Parse(Сonfiguration.IPAddress), Сonfiguration.Port);
+                listner.Start();
+                while (true)
+                    Start(listner.AcceptTcpClient()).Wait();
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(ex, ex.Message);
+            }
         }
         private async Task Start(TcpClient Client)
         {
+            try
+            { 
+                client = Client;
+                Log.LogInformation($"Connect client IP:{((IPEndPoint)client.Client.RemoteEndPoint).Address}");
 
-            client = Client;
-            Log.LogInformation($"Connect client IP:{((IPEndPoint)client.Client.RemoteEndPoint).Address}");
+                //Псевдо Балансировщик нагрузки
+                Server s;
+                s = Сonfiguration.Servers.OrderBy(x => x.CountrediRection).First();
 
-            //Псевдо Балансировщик нагрузки
-            Server s;
-            s = Сonfiguration.Servers.OrderBy(x => x.CountrediRection).First();
-
-            //Переадресация данных
-            Log.LogInformation($"Client IP:{((IPEndPoint)client.Client.RemoteEndPoint).Address} redirect to IP:{s.IPAdress} Port{s.Port}");
-            await Redirect(s.IPAdress, s.Port);
-            s.CountrediRection = ++s.CountrediRection;
+                //Переадресация данных
+                Log.LogInformation($"Client IP:{((IPEndPoint)client.Client.RemoteEndPoint).Address} redirect to IP:{s.IPAdress} Port{s.Port}");
+                await Redirect(s.IPAdress, s.Port);
+                s.CountrediRection = ++s.CountrediRection;
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(ex, ex.Message);
+            }
         }
         private async Task Redirect(string address, int port)
         {
-            await server.ConnectAsync(address, port);
-            Task T1 = Task.Factory.StartNew(async () => { await RedirectTask(client, server); });
-            Task T2 = Task.Factory.StartNew(async () => { await RedirectTask(server, client); });
+            try
+            { 
+                server = new();
+                await server.ConnectAsync(address, port);
+                Task T1 = Task.Factory.StartNew(async () => { await RedirectTask(client, server); });
+                await Task.Factory.StartNew(async () => { await RedirectTask(server, client); });
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(ex, ex.Message);
+            }
         }
 
         private async Task RedirectTask(TcpClient target, TcpClient destination)
         {
-            int ByteTotal=0;
-            int bytesRead;
-            byte[] recvbuf = new byte[8192];
-            do
-            {
-                bytesRead = await target.GetStream().ReadAsync(recvbuf.AsMemory(0, recvbuf.Length));
-                await destination.GetStream().WriteAsync(recvbuf.AsMemory(0, bytesRead));
-                ByteTotal += bytesRead;
-            } while (bytesRead != 0);
+            try
+            { 
+                int ByteTotal=0;
+                int bytesRead;
+                byte[] recvbuf = new byte[8192];
+                do
+                {
+                    bytesRead = await target.GetStream().ReadAsync(recvbuf.AsMemory(0, recvbuf.Length));
+                    await destination.GetStream().WriteAsync(recvbuf.AsMemory(0, bytesRead));
+                    ByteTotal += bytesRead;
+                } while (target.GetStream().DataAvailable);
 
-            Log.LogInformation
-                ($"IP:{((IPEndPoint)target.Client.RemoteEndPoint).Address}" +
-                $" Port:{((IPEndPoint)target.Client.RemoteEndPoint).Port}" +
-                $" send to IP:{((IPEndPoint)destination.Client.RemoteEndPoint).Address}" +
-                $" Port:{((IPEndPoint)destination.Client.RemoteEndPoint).Port} bytes:{ByteTotal}");
+                Log.LogInformation
+                    ($"IP:{((IPEndPoint)target.Client.RemoteEndPoint).Address}" +
+                    $" Port:{((IPEndPoint)target.Client.RemoteEndPoint).Port}" +
+                    $" send to IP:{((IPEndPoint)destination.Client.RemoteEndPoint).Address}" +
+                    $" Port:{((IPEndPoint)destination.Client.RemoteEndPoint).Port} bytes:{ByteTotal}");
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(ex, ex.Message);
+            }
         }
     }
 }
