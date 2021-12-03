dfc42b9eefd35e84aff7f3d48996fc935264689c
boevas <boev_a@bk.ru>
2021-12-03 18:20:57 +0300

    SimMetricsCore

---
+++ SimMetricsCore/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using SimMetricsCore.API;
+namespace ConsoleApp6
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subclassTypes = Assembly
+           .GetAssembly(typeof(AbstractStringMetric))
+           .GetTypes()
+           .Where(t => t.IsSubclassOf(typeof(AbstractStringMetric)));
+
+            string str1 = "добрый день";
+            string str2 = "добрый чудесный весенний день";
+            foreach (Type t in subclassTypes)
+            {
+                AbstractStringMetric instance = (AbstractStringMetric)Activator.CreateInstance(t);
+                double res = instance.GetSimilarity(str1, str2);
+                //if(res > 0.7 || res < 0.3)
+                    Console.WriteLine($"{t.Name} :{res}");
+            }
+            
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
